989fb4acc09a62133f5cb745db67a0c0d2374864
Gouri Shankar <gourishankar@Gouris-MacBook-Air.local>
2024-08-24 08:24:51 +0530

    first commit

---
+++ server/utils/sendEmail.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer");
+
+module.exports = async (email, subject, text) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail", // Use Gmail service
+      auth: {
+        user: process.env.EMAIL_USER, // Your Gmail address from environment variables
+        pass: process.env.EMAIL_PASS, // Your Gmail password from environment variables
+      },
+    });
+
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: subject,
+      text: text,
+    });
+    console.log("email sent successfully");
+  } catch (error) {
+    console.log("email not sent!");
+    console.log(error);
+    return error;
+  }
+};
